32358b8a21d077943da299d20015627af605a5be
Tomasz Kłak <tomasz.klak@nordsec.com>
2025-05-30 13:45:47 +0200

    Print stats for various sizes of diffs

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import argparse
 import datetime
+import requests
 from github import Auth, Github
 from yaspin import yaspin
 
 def percentile(p, l):
     idx = int((float(p)/100.0)*len(l))
     return l[idx][0]
+
+
+def count_lines_from_url(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error for bad responses (4xx and 5xx)
+    content = response.text
+    lines = content.splitlines()
+    return len(lines)
 
 @yaspin(text="Loading pull requests...")
 def get_merged_pulls(repo, last_days):
@@ -18,9 +27,32 @@ def get_merged_pulls(repo, last_days):
             if pull.is_merged():
                 if pull.created_at < deadline:
                     return ret
-                ret.append(pull)
+                size = count_lines_from_url(pull.diff_url)
+                ret.append((pull, size))
         page += 1
     return ret
+
+def print_stats(pulls, min_size=None, max_size=None):
+    print()
+    if not min_size and not max_size:
+        print(f"{len(pulls)} pull requests in the last {args.days} days for {args.repository}")
+    elif not min_size:
+        pulls = [(d,p,s) for (d,p,s) in pulls if s < max_size]
+        print(f"{len(pulls)} pull requests in the last {args.days} days for {args.repository} and max diff size {max_size} lines")
+    elif not max_size:
+        pulls = [(d,p,s) for (d,p,s) in pulls if s >= min_size]
+        print(f"{len(pulls)} pull requests in the last {args.days} days for {args.repository} and min diff size {min_size} lines")
+    else:
+        pulls = [(d,p,s) for (d,p,s) in pulls if s >= min_size and s < max_size]
+        print(f"{len(pulls)} pull requests in the last {args.days} days for {args.repository} and diff size from {min_size} up to {max_size} lines")
+        
+    avg = sum([d.total_seconds() for (d,_,_) in pulls]) / len(pulls)
+    print(" median:", pulls[int(len(pulls)/2)][0])
+    print("    60%:", percentile(60, pulls))
+    print("    70%:", percentile(70, pulls))
+    print("    80%:", percentile(80, pulls))
+    print("    90%:", percentile(90, pulls))
+    print("average:", datetime.timedelta(seconds=avg))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -36,18 +68,16 @@ if __name__ == "__main__":
 
     repo = g.get_repo(args.repository)
     pulls = get_merged_pulls(repo, args.days)
-    pulls = [(p.merged_at-p.created_at, p) for p in pulls]
+    pulls = [(p.merged_at-p.created_at, p, size) for (p,size) in pulls]
     pulls.sort()
-    for (duration,pull) in pulls:
-        print(f"Pull {pull.number} merged in {duration}")
+    for (duration,pull,size) in pulls:
+        print(f"Pull {pull.number} ({size} lines) merged in {duration}")
 
-    avg = sum([d.total_seconds() for (d,_) in pulls]) / len(pulls)
+    avg = sum([d.total_seconds() for (d,_,_) in pulls]) / len(pulls)
     
-    print()
-    print(f"{len(pulls)} pull requests in the last {args.days} days for {args.repository}")
-    print(" median:", pulls[int(len(pulls)/2)][0])
-    print("    60%:", percentile(60, pulls))
-    print("    70%:", percentile(70, pulls))
-    print("    80%:", percentile(80, pulls))
-    print("    90%:", percentile(90, pulls))
-    print("average:", datetime.timedelta(seconds=avg))
+    print_stats(pulls)
+    print_stats(pulls, max_size=20)
+    print_stats(pulls, min_size=20, max_size=50)
+    print_stats(pulls, min_size=50, max_size=200)
+    print_stats(pulls, min_size=200, max_size=500)
+    print_stats(pulls, min_size=500)
